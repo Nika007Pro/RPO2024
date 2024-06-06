b262d2ffc6b4bdaaf887e9c5a6702e65235382c6
Vladimir Mumin <vovanmumin@gmail.com>
2024-06-06 11:52:10 +0300

    laba 2 and 3

--- app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,20 +1,49 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+import java.nio.charset.StandardCharsets;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TransactionEvents{
+
 
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
+    private String pin;
+    public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key,byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
+    public native boolean transaction(byte[] trd);
+
+
+    ActivityResultLauncher activityResultLauncher;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +51,106 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        int res = initRng();
+        byte[] v = randomBytes(10);
 
         // Example of a call to a native method
-        TextView tv = binding.sampleText;
+        TextView tv = binding.sampleButton;
         tv.setText(stringFromJNI());
+
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>(){
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
+                    }
+                });
     }
 
-    /**
-     * A native method that is implemented by the 'fclient' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public void onButtonClick(View v)
+    {
+//        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+//        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+//        byte[] dec = decrypt(key, enc);
+//        String s = new String(Hex.encodeHex(dec)).toUpperCase();
+//        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+
+        // print PinCode after like push notification
+
+//        Intent it = new Intent(this, PinpadActivity.class);
+//        startActivity(it);
+//        activityResultLauncher.launch(it);
+
+//
+//        new Thread(()-> {
+//            try {
+//                byte[] trd = stringToHex("9F0206000000000100");
+//                boolean ok = transaction(trd);
+//                runOnUiThread(()-> {
+//                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+//                });
+//
+//            } catch (Exception ex) {
+//                // todo: log error
+//            }
+//        }).start();
+
+        byte[] trd = stringToHex("9F0206000000000100");
+        boolean ok = transaction(trd);
+    }
+
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(()-> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+        Intent it = new Intent(MainActivity.this, PinpadActivity.class);
+        it.putExtra("ptc", ptc);
+        it.putExtra("amount", amount);
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(it);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception ex) {
+                //todo: log error
+            }
+        }
+        return pin;
+    }
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
+
+
 }
